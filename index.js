88064970af07f4cd0f126c92ec55b5708f392ab9
Georg Treitz <55241447+GfsTee@users.noreply.github.com>
2020-12-21 13:04:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 5000
+const PORT = process.env.PORT ||5000
 const fs = require('fs');
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
